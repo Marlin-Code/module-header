bc8ef0cb0324cea7ad523082a52cb27d7b3d5439
George Cooper <george@marlincode.com>
2023-02-22 11:36:56 -0500

    look a little better

--- src/template/components/Header/HeaderMenuItem/HeaderMenuItem.jsx
+++ src/template/components/Header/HeaderMenuItem/HeaderMenuItem.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './HeaderMenuItem.css';
 
-function HeaderMenuItem({text, key}) {
+function HeaderMenuItem({text, index, totalItems}) {
+  console.log(index)
   return (
-    <div className="header_item" key={key}>
+    <div className="header_item" key={index}>
       <span className="header_item_text">{text}</span>
     </div>
   )
